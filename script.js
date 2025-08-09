62279354512174f2d91c6432f4204fd095bf95fe
MDeak97 <michaeldeyak90@gmail.com>
2025-08-09 17:04:16 +1000

    Answering Q 2.1

---
+++ script.js
@@ -0,0 +1,12 @@
+// File: script.js
+// Description: File to answer question from Part 2 of assessment JSAT 2
+// Author: Michael Deak
+// Date: 09/08/2025
+// Version: 1.00
+
+// Q2.1 - Build an object called My Movie
+let myMovie = {
+  Title: "The longest day",
+  Year: "1964",
+};
+console.log(myMovie);

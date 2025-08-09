9eba61161e997abd7bcfdf09cb79c1217a55dc4f
MDeak97 <michaeldeyak90@gmail.com>
2025-08-09 18:31:12 +1000

    Update script.js

--- part3/script.js
+++ part3/script.js
@@ -3,3 +3,10 @@
 // Author: Michael Deak
 // Date: 09/08/2025
 // Version: 1.00
+
+// Q3.1 - Define strings
+let myString = "This is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Michael";
+console.log(myString, anotherString, hello, myName);

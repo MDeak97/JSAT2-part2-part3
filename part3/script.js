e4720432dd681f8b738a44b9812f115389d75f94
MDeak97 <michaeldeyak90@gmail.com>
2025-08-10 00:08:50 +1000

    Update script.js

--- part3/script.js
+++ part3/script.js
@@ -1,7 +1,7 @@
 // File: script.js
 // Description: File to answer questions from Part 3 of assessment JSAT 2
 // Author: Michael Deak
-// Date: 09/08/2025
+// Date: 10/08/2025
 // Version: 1.00
 
 // Q3.1 - Define strings

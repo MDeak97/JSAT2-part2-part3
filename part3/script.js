6a9118e192f1449dd2ce45ca07e57a3001c5b4c3
MDeak97 <michaeldeyak90@gmail.com>
2025-10-24 15:53:23 +1100

    Update Q3.2

--- part3/script.js
+++ part3/script.js
@@ -18,7 +18,7 @@ console.log(myString.length);
 // Finding first character which is T
 myString.charAt(0);
 console.log(myString.charAt(0));
-// Finding 11th character which is i
+// Finding 11th character which is t
 myString.charAt(13);
 console.log(myString.charAt(11));
 
